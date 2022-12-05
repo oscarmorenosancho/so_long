25a47e7421f8d1007dac124693b67e2ced55f770
Oscar Moreno Sancho <omoreno-@car7s4.42barcelona.com>
2022-12-05 16:08:26 +0100

    xpm for animations added

--- src_bonus/so_long_bonus.h
+++ src_bonus/so_long_bonus.h
@@ -6,15 +6,15 @@
 /*   By: omoreno- <omoreno-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 11:37:53 by omoreno-          #+#    #+#             */
-/*   Updated: 2022/12/05 10:51:23 by omoreno-         ###   ########.fr       */
+/*   Updated: 2022/12/05 15:07:33 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_BONUS_H
 # define SO_LONG_BONUS_H
 
-# define BLOCK_WIDTH	40
-# define BLOCK_HEIGHT	40
+# define BLOCK_WIDTH	64
+# define BLOCK_HEIGHT	64
 # include "../src_common/common.h"
 
 #endif
